30f11b4418a574ff091313bac0f7fa2cdc810603
Nick Kuizenga <nickkuis@hotmail.com>
2018-12-16 21:45:41 -0500

    fixed pagename in save pagepost functionality

--- insta/urls.py
+++ insta/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post/$', views.GetPostImg.as_view(), name='get_post'),
     url(r'^page/$', views.GetPage.as_view(), name='get_page'),
-    url(r'^savepost/(?P<page>.+)/(?P<img>.+)/$', views.SavePost.as_view(), name="save_post")
+    url(r'^savepost/(?P<page>[-\w]+)/(?P<img>.+)/$', views.SavePost.as_view(), name="save_post")
 ]
