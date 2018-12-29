b214df8c17eb76405c8ada820d85f7a553668b20
Nick Kuizenga <nickkuis@hotmail.com>
2018-12-29 13:39:06 -0500

    customized admin panel

--- insta/admin.py
+++ insta/admin.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import IgPage, IgPost, SavePagePost
+
 # Register your models here.
+
+class IgPageAdmin(admin.ModelAdmin):
+	list_display = ('page','url')
+
+class IgPostAdmin(admin.ModelAdmin):
+	list_display = ('page','url', 'img')
+
+
+class SavePagePostAdmin(admin.ModelAdmin):
+	list_display = ('page','url', 'img')
+
+admin.site.register(IgPage, IgPageAdmin)
+
+admin.site.register(IgPost, IgPostAdmin)
+
+admin.site.register(SavePagePost, SavePagePostAdmin)
